7faae15bccb9ec7c8125c5e430d0b085c1508a1f
Marco Orban <marco.orban@gmail.com>
2025-08-18 17:12:03 +0800

    Update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,12 @@
+set -g fish_greeting
+
+fish_config theme choose Dracula
+
 if test (uname) = "Darwin"
     if test -d /opt/homebrew/bin
         fish_add_path /opt/homebrew/bin
         fish_add_path /opt/hombrew/sbin
     end
 end
+
+alias fsrc="source ~/.config/fish/config.fish"
