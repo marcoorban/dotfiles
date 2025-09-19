474cee1c531cdf176a333ba9af7a74586c171cc6
morban <marco.orban@gmail.com>
2025-09-19 19:37:03 +0800

    Added env variables to fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,8 +1,11 @@
 set -g fish_greeting
+set -gx EDITOR (which nvim)
+set -gx VISUAL (which nvim)
+set -gx SHELL (which fish)
 
 fish_config theme choose Dracula
 
-if test (uname) = "Darwin"
+if test (uname) = Darwin
     if test -d /opt/homebrew/bin
         fish_add_path /opt/homebrew/bin
         fish_add_path /opt/hombrew/sbin
@@ -11,7 +14,7 @@ end
 
 alias fsrc="source ~/.config/fish/config.fish"
 
-source ~/.config/.myenv.sh
-
 export XCURSOR_THEME=Bibata-Modern_Ice
 export XCURSOR_SIZE=24
+
+set -gx HYPRSHOT_DIR "~/Pictures/Screenshots"
