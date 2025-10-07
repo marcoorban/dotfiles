4cf6f9471960c57a89f75f07be37422661a05370
marcoorban <marco.orban@gmail.com>
2025-10-07 19:59:05 +0800

    deleted hyprshade

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Enable spell checking automatically for .tex and .txt files
+--
+vim.api.nvim_set_hl(0, "VirtColumn", { bg = "#802080" })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "tex", "text", "markdown", "plaintex" },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = { "en_us" } -- add more like {"en_us", "es"} if needed
+
+    -- Set text width and color column at 80
+    vim.opt_local.textwidth = 80
+    vim.opt_local.colorcolumn = "80"
+
+    -- Make formatting behave nicely in prose
+    vim.opt_local.formatoptions:append("t")
+  end,
+})
---
+++ dot_config/nvim/lua/plugins/empty_virt-columns.lua
@@ -0,0 +1,8 @@
+return {
+  "lukas-reineke/virt-column.nvim",
+  opts = {
+    char = "│", -- thin vertical bar
+    highlight = "VirtColumn",
+    virtcolumn = "80", -- same as colorcolumn
+  },
+}
